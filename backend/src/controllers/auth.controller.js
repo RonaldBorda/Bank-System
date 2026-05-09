0df0a88df72e1fb0e76e4c7c13779089cab4a8d7
RonaldBorda <ronald.bordab@gmail.com>
2026-05-08 19:19:22 -0500

    ruta login

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
 const register = async (req, res) => {
@@ -38,7 +39,73 @@ const register = async (req, res) => {
     }
 
 };
+const login = async (req, res) => {
+
+    try {
+
+        const { correo, password } = req.body;
+
+        const user = await User.findOne({ correo });
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'Usuario no encontrado'
+            });
+        }
+
+        if (user.bloqueado) {
+            return res.status(403).json({
+                message: 'Cuenta bloqueada'
+            });
+        }
+
+        const passwordCorrect = await bcrypt.compare(password, user.password);
+
+        if (!passwordCorrect) {
+
+            user.intentosFallidos += 1;
+
+            if (user.intentosFallidos >= 5) {
+                user.bloqueado = true;
+            }
+
+            await user.save();
+
+            return res.status(401).json({
+                message: 'Contraseña incorrecta',
+                intentosRestantes: 5 - user.intentosFallidos
+            });
+        }
+
+        user.intentosFallidos = 0;
+
+        await user.save();
+
+        const token = jwt.sign(
+            {
+                id: user._id
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: '2h'
+            }
+        );
+
+        res.json({
+            message: 'Login exitoso',
+            token
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
+
+};
 
 module.exports = {
-    register
+    register, login
 };
