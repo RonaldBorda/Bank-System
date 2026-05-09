4e7c4cbb3ebf87f0ffa8a7242ac85e860cf6f44f
RonaldBorda <ronald.bordab@gmail.com>
2026-05-09 12:12:09 -0500

    interfaz login funcional

--- frontend/frontend-bank/src/app/pages/login/login.ts
+++ frontend/frontend-bank/src/app/pages/login/login.ts
@@ -1,9 +1,45 @@
 import { Component } from '@angular/core';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ 
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  correo = '';
+  password = '';
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+  login() {
+    const data = {
+      correo: this.correo,
+      password: this.password
+    };
+    this.authService.login(data).subscribe({
+      next: (response: any) => {
+        localStorage.setItem('token', response.token);
+        this.router.navigate(['/dashboard']);
+      },
+      error: (error) => {
+        console.log(error);
+        alert('Credenciales incorrectas');
+      }
+    });
+  
+  }
+}
+
