8321358b966bfa9f6ee032cc9866adc9fe571fa8
RonaldBorda <ronald.bordab@gmail.com>
2026-05-12 18:02:34 -0500

    arreglos

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const accountRoutes = require('./routes/account.routes');
 const transactionRoutes = require('./routes/transaction.routes');
 const paymentRoutes = require('./routes/payment.routes');
 const app = express();
-
+ 
 app.use(cors());
 app.use(express.json()); 
 app.use('/api', testRoutes);
